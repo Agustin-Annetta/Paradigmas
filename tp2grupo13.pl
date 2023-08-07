0956ba6a475dbe6ab0cb580e5877f6d879b5688e
Annetta-Agustín <aannetta@frba.utn.edu.ar>
2023-08-07 20:28:58 -0300

    tp2grupo13.pl

--- tp2grupo13.pl
+++ tp2grupo13.pl
@@ -21,10 +21,35 @@ pesoHormiga(2).
 peso(pumba, 100). 
 peso(timon, 50). 
 peso(simba, 200). 
+peso(scar, 300). 
+peso(shenzi, 400). 
+peso(banzai, 500). 
+
 
 %1) a) Qué cucaracha es jugosita: ó sea, hay otra con su mismo tamaño pero ell es más gordita. 
 
 mismoTamanio(Cucaracha1, Cucaracha2):-((_,Tamanio1,_)==(_,Tamanio2,_)).
+esMasGorda(Cucaracha1, Cucaracha2):-((_,_,Peso1)>(_,_,Peso2)).
 
-%jugosita(Cucaracha):-
+jugosita(Cucaracha):-mismoTamanio(Cucaracha, OtraCucaracha), esMasGorda(Cucaracha, OtraCucaracha).
+
+
+%b) Si un personaje es hormigofílico... (Comió al menos dos hormigas). 
+
+hormigofilico(Personaje):-
+    comio(Personaje,hormiga(Hormiga1)),comio(Personaje, hormiga(Hormiga2)).
+
+%c) Si un personaje es cucarachofóbico (no comió cucarachas). 
+
+cucarachofóbico(Personaje):-not(comio(Personaje,cucaracha)).
+
+%d) Conocer al conjunto de los picarones. Un personaje es picarón si comió una cucaracha jugosita ó si se come a Remeditos la vaquita. Además, pumba es picarón de por sí. 
+
+%2
+
+persigue(scar, timon). 
+persigue(scar, pumba). 
+persigue(shenzi, simba). 
+persigue(shenzi, scar). 
+persigue(banzai, timon) 
 
